144576e831894778a83362a4fe9f434ba0f2a08b
GGin <15929667995@163.com>
2026-03-26 00:23:42 +0800

    node_pdf_to_md.py:pdf转md节点

--- app/import_process/agent/nodes/node_pdf_to_md.py
+++ app/import_process/agent/nodes/node_pdf_to_md.py
@@ -1,7 +1,12 @@
 import sys
+from pathlib import Path
 
 from app.core.logger import logger
 from app.import_process.agent.state import ImportGraphState
+from app.utils.path_util import PROJECT_ROOT
+from app.utils.task_utils import add_running_task, add_done_task
+
+
 # 流程
 # 主函数：
 #  def node_pdf_to_md:
@@ -27,9 +32,22 @@ from app.import_process.agent.state import ImportGraphState
 def step1_validate_param(state:ImportGraphState):
     #       非空校验
     pdf_path = state["pdf_path"]
+    local_dir = state["local_dir"]
+    if not pdf_path:
+        raise ValueError(f"[step1_validate_param] 文件参数错误{state}")
+    if not local_dir:
+        local_dir = PROJECT_ROOT / "output"
     #       文件校验
+    pdf_path_obj = Path(pdf_path)
+    local_dir_obj = Path(local_dir)
+    if not pdf_path_obj.exists():
+        # 如果文件不存在直接报错
+        raise FileNotFoundError(f"[step1_validate_param] 文件不存在{state}")
+    if not local_dir_obj.exists():
+        # 如果工作目录不存在，直接创建工作目录
+        local_dir_obj = local_dir_obj.mkdir(parents=True,exist_ok=True)
 
-    pass
+    return pdf_path_obj,local_dir_obj
 
 
 def step2_upload_pdf():
@@ -51,13 +69,26 @@ def node_pdf_to_md(state: ImportGraphState) -> ImportGraphState:
     """
 
     #       日志和任务状态
+    # 1. 接口开始的日志输出
+    task_id = state['task_id']
+    function_name = sys._getframe().f_code.co_name
+    logger.info(f">>> {function_name} 节点开始执行，现在状态为: {state}")
+    add_running_task(task_id, function_name)
+    try:
+        #       step1_validate_param: 校验参数
+        pdf_path_obj, local_dir_obj = step1_validate_param(state)
+        #       step2_upload_pdf: 申请、上传pdf文件
+        zip_url = step2_upload_pdf(pdf_path_obj)
+        #       step3_download_zipfile: 下载解压minerU返回的 解压文件
+        zip_file = step3_download_unzip(zip_url)
+        return state
+    except Exception as e:
+        raise e
+    finally:
+        # 4. 节点结束的日志输出
+        logger.info(f">>> {function_name} 节点执行结束,节点状态: {state}")
+        add_done_task(task_id, function_name)
 
-    #       step1_validate_param: 校验参数
-    step1_validate_param(state)
-    #       step2_upload_pdf: 申请、上传pdf文件
-    step2_upload_pdf()
-    #       step3_download_zipfile: 下载解压minerU返回的 解压文件
-    step3_download_unzip()
-    #       日志和任务状态
-    logger.info(f">>> [Stub] 执行节点: {sys._getframe().f_code.co_name}")
-    return state
+
+
+
