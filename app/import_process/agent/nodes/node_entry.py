1ce52207d3042f4c7bb8576ccf1790d0fb8c6dee
GGin <15929667995@163.com>
2026-03-25 23:47:31 +0800

    node_entry节点

--- app/import_process/agent/nodes/node_entry.py
+++ app/import_process/agent/nodes/node_entry.py
@@ -1,6 +1,5 @@
+import os.path
 import sys
-
-from langgraph.constants import END
 
 from app.core.logger import logger
 from app.import_process.agent.state import ImportGraphState
@@ -16,24 +15,29 @@ def node_entry(state: ImportGraphState) -> ImportGraphState:
     2. 判断文件类型 (PDF/MD)。
     3. 设置 state 中的路由标记 (is_pdf_read_enabled / is_md_read_enabled)。
     """
+    # 1. 接口开始的日志输出
     task_id = state['task_id']
-    function_name  = sys._getframe().f_code.co_name
-    logger.info(f">>> {function_name} 执行节点: {sys._getframe().f_code.co_name}")
-    add_running_task(task_id)
-    #1. 接口开始的日志输出
-    #2. 校验数据: local_file_path
+    function_name = sys._getframe().f_code.co_name
+    logger.info(f">>> {function_name} 节点开始执行，现在状态为: {state}")
+    add_running_task(task_id, function_name)
+
+    # 2. 校验数据: local_file_path
     local_file_path = state['local_file_path']
     if not local_file_path:
-        return END
-    #3. 记录任务状态
+        return state
+    # 3. 记录任务状态
     if local_file_path.endswith('.pdf'):
         state['is_pdf_read_enabled'] = True
+        state["pdf_path"] = local_file_path
     elif local_file_path.endswith('.md'):
         state['is_md_read_enabled'] = True
+        state["md_path"] = local_file_path
     else:
-        logger.error()
-        raise Exception()
-    #4. 节点结束的日志输出
-    logger.info(f">>> {function_name} 执行节点: {sys._getframe().f_code.co_name}")
-    add_done_task(task_id,function_name)
+        logger.error(f">>> {function_name} 节点文件格式非md/pdf，无法继续解析")
+    # 提取文件名，防止后续没有解析出file_item
+    file_name = os.path.basename(local_file_path).split(".")[0]
+    state["file_title"] = file_name
+    # 4. 节点结束的日志输出
+    logger.info(f">>> {function_name} 节点执行结束,节点状态: {state}")
+    add_done_task(task_id, function_name)
     return state
